5bb157d88dc914e840773cb3d0fe46858d891beb
laiyq <laiyq@dfc.cn>
2015-10-28 13:17:48 +0800

    init for v2

--- lib/aliyun_open_search.rb
+++ lib/aliyun_open_search.rb
@@ -3,6 +3,9 @@ require "aliyun_open_search/base"
 require "aliyun_open_search/syncs"
 require "aliyun_open_search/search"
 require "aliyun_open_search/scan"
+require "aliyun_open_search/searchable"
+require "aliyun_open_search/extend"
 
 module AliyunOpenSearch
+  ActiveRecord::Base.extend AliyunOpenSearch::Extend::Searchable
 end
