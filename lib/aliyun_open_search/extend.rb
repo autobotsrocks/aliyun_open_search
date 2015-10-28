18a1158fa81b6945d6b0422d7c7034655a63550e
laiyq <laiyq@dfc.cn>
2015-10-28 18:06:58 +0800

    fix

--- lib/aliyun_open_search/extend.rb
+++ lib/aliyun_open_search/extend.rb
@@ -1,14 +1,14 @@
 module AliyunOpenSearch
   module Extend
     module Searchable
-      def acts_as_searchable(*fields)
+      def acts_as_searchable(fields, app_name, table_name=nil)
         require 'aliyun_open_search/searchable'
         include AliyunOpenSearch::Searchable
         
         class_eval do
           class_attribute :app_name, :app_table_name, :app_fields
-          self.app_name = 'rds_sjll_teacher'
-          self.app_table_name = :teachers
+          self.app_name = app_name
+          self.app_table_name = table_name || self.table_name.to_sym
           self.app_fields = fields
         end
       end
