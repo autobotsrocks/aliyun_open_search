4e6254d02161bb821f51b9ba15687f9f7bde798b
laiyq <laiyq@dfc.cn>
2015-10-28 17:59:42 +0800

    fix

--- lib/aliyun_open_search/extend.rb
+++ lib/aliyun_open_search/extend.rb
@@ -4,6 +4,13 @@ module AliyunOpenSearch
       def acts_as_searchable(*fields)
         require 'aliyun_open_search/searchable'
         include AliyunOpenSearch::Searchable
+        
+        class_eval do
+          class_attribute :app_name, :app_table_name, :app_fields
+          self.app_name = 'rds_sjll_teacher'
+          self.app_table_name = :teachers
+          self.app_fields = fields
+        end
       end
     end
   end
--- lib/aliyun_open_search/searchable.rb
+++ lib/aliyun_open_search/searchable.rb
@@ -6,7 +6,24 @@ module AliyunOpenSearch
     
     module ClassMethods
       def update_opensearch
-        
+        params = {
+          action: :push,
+          table_name: self.app_table_name,
+          items: []
+        }
+        self.unscoped.all.each do |t|
+          params[:items] << if t.is_online then
+            {
+              cmd: 'update',
+              fields: self.app_fields.inject(Hash.new){|h,f| h.merge({f => t.send(f)})}
+            }
+          else
+            {cmd: 'delete', fields: {id: t.id}}
+          end
+        end
+        puts "params size = #{params.to_json.size}"
+        response = AliyunOpenSearch::Syncs.new(self.app_name).execute(params)
+        response_body = JSON.load(response.body)
       end
       
       def opensearch(keyword, options={})
